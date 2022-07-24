55d725c8d28b6ca72ff0600ee5e8b52939a12d76
israel386 <israel_delarosa@knights.ucf.edu>
2022-07-24 19:13:28 -0400

    israel

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,10 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(
-    process.env.DB_NAME, 
-    process.env.DB_USER, 
-    process.env.DB_PASSWORD, 
-    
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+
     {
       host: 'localhost',
       dialect: 'mysql',
