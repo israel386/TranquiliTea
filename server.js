d6399f52b3db586eb1a01aa7c17e5f7cf2a970cf
GelP1987 <101074657+GelP1987@users.noreply.github.com>
2022-07-24 17:13:36 -0400

    added handlerbars and express handlebars packages as well as test text for proof of concept, main and homepage handlabars scaffolsing set

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const express = require('express');
 const routes = require('./controllers');
 // imported sequelize from connection.js file 
 const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars")
+const path = require("path")
+const helpers = require("./utils/helpers")
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+const hbs = exphbs.create({ helpers })
 app.use(express.urlencoded({ extended: true }));
-
+app.engine("handlebars", hbs.engine)
 app.use(routes);
-
+app.use(express.static(path.join(__dirname, "public")))
+app.set("view engine", "handlebars")
 // sync sequelize models to the database, then turn on the server
 
 sequelize.sync({ force: false }).then(() => {
