447009331baa9e67a930c78b1651141576cd5f95
israel386 <israel_delarosa@knights.ucf.edu>
2022-07-27 16:37:14 -0400

    no change

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,11 @@ const router = require("express").Router();
 const affirmationsRoutes = require("./affirmations-routes");
 const quotesRoutes = require("./quotes-routes");
 const teasRoutes = require("./teas-routes");
-const userRouted = require("./user-routes")
+const userRoutes = require("./user-routes")
 
 router.use("/affirmations", affirmationsRoutes);
 router.use("/quotes", quotesRoutes);
 router.use("/teas", teasRoutes);
+router.use("/user", userRoutes);
 
 module.exports = router;
